1458eaea769807c8d8cf9accec00559fdf94425e
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2024-10-20 20:49:26 -0300

    RegistrationEntity

---
+++ src/main/java/br/com/alura/ProjetoAlura/registration/Registration.java
@@ -0,0 +1,64 @@
+package br.com.alura.ProjetoAlura.registration;
+
+import java.time.LocalDate;
+
+import br.com.alura.ProjetoAlura.course.Course;
+import br.com.alura.ProjetoAlura.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Registration {
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Course course;
+
+    private LocalDate enrollmentDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+
+	public LocalDate getEnrollmentDate() {
+		return enrollmentDate;
+	}
+
+	public void setEnrollmentDate(LocalDate enrollmentDate) {
+		this.enrollmentDate = enrollmentDate;
+	}
+    
+    
+
+
+
+}
