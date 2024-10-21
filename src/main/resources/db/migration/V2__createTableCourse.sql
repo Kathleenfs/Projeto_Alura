0179bd72734f9f957d5390d1623a4f66b26b462d
KATHLEEN FERREIRA DOS SANTOS SILVA <90014122+Kathleenfs@users.noreply.github.com>
2024-10-20 21:05:34 -0300

    CreateTableRegistration-V2

--- src/main/resources/db/migration/V2__createTableCourse.sql
+++ src/main/resources/db/migration/V2__createTableCourse.sql
@@ -7,3 +7,19 @@ CREATE TABLE courses (
     status VARCHAR(10),
     inactivationDate DATE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
+
+
+CREATE TABLE registration (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    course_id BIGINT NOT NULL,
+    enrollmentDate DATE NOT NULL,
+
+    CONSTRAINT fk_registration_user FOREIGN KEY (user_id)
+        REFERENCES user(id) ON DELETE CASCADE,
+ 
+    CONSTRAINT fk_registration_courses FOREIGN KEY (course_id)
+        REFERENCES courses(id) ON DELETE CASCADE,
+
+    CONSTRAINT unique_user_courses UNIQUE (user_id, course_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
